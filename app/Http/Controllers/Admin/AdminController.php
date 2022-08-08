d0796485610d231b7f55f51ac312d8764ca160d0
ashard66 <18102007@ittelkom-pwt.ac.id>
2022-08-09 03:03:15 +0700

    [BE] Konfigurasi ambil data dari tabel order detail untuk membuat grafik penjualan

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\User;
 use App\Models\Product;
+use App\Charts\SalesChart;
 use App\Models\OrderDetail;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
+    protected $salesChart;
+
+    public function __construct(SalesChart $salesChart)
+    {
+        $this->salesChart = $salesChart;
+    }
     public function index()
     {
         $data['total_product'] = Product::count();
@@ -19,6 +26,7 @@ class AdminController extends Controller
         $data['total_completed'] = OrderDetail::where('status',3)->whereMonth('created_at', Date('m'))->whereYear('created_at',Date('Y'))->count();
         $data['total_order'] = $data['total_pending'] + $data['total_shipping'] + $data['total_completed'];
         $data['last_order'] = OrderDetail::orderBy('id','DESC')->limit(5)->get();
+        $data['chart'] = $this->salesChart->build();
         return view('dashboard.index', compact('data'));
     }
 }
--- resources/views/layouts/dashboard/master.blade.php
+++ resources/views/layouts/dashboard/master.blade.php
@@ -124,6 +124,8 @@
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
     <script src="{{ asset('admin/js/soft-ui-dashboard.min.js?v=1.0.6') }}"></script>
+    <script src="{{ LarapexChart::cdn() }}"></script>
+
     @stack('js')
 </body>
 
